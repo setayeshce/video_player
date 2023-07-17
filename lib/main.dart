42947ac53b86c5ab68480ccb6b7ad9f0127c6c09
setayesh hoseiini <setayesh.hoseiini79@gmail.com>
2023-07-17 12:57:45 +0330

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:math';
 import 'dart:ui';
-
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus
-import path_provider_foundation
-import sqflite
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
